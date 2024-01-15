8b97b449c16f6fe79b1a74ebef60bdf655d7478e
tonytangdev <tony.tang.developer@gmail.com>
2024-01-15 14:33:13 +0100

    feat: implemented upload function inside upload lambda

--- backend/src/handlers/upload.ts
+++ backend/src/handlers/upload.ts
@@ -1,10 +1,22 @@
 import { APIGatewayProxyEventV2, Handler } from "aws-lambda";
+import { uploadFile } from "../helpers/s3";
 
 export const handler: Handler<APIGatewayProxyEventV2> = async (event) => {
+  const { body } = event;
+  if (!body) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "No file provided",
+      }),
+    };
+  }
+
+  // string base64 -> Buffer
+  const buffer = Buffer.from(body, "base64");
+  await uploadFile("img.png", buffer);
+
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: "Hello World!",
-    }),
   };
 };
